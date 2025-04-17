2fd145456bd97bf3c58b4323009a52918f7d38ee
skyler sora <1304415475@qq.com>
2025-04-17 09:21:05 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Define a route for comments
+app.get('/comments', (req, res) => {
+    res.send('List of comments');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
